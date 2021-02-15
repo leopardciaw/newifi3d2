64417e11b086ff02c79e576a303ae5c67e782d13
leopardciaw <quanyongw@gmail.com>
2021-02-15 16:48:06 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/Newifi D2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
 
 
 
@@ -40,7 +40,7 @@ git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-th
 sed -i "s/OpenWrt /Leopard build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 
 
